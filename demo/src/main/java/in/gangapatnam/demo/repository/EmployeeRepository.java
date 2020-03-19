65ce504cc133385d67dcc1515eabb9a6d1b25047
javab <javab@192.168.248.1>
2020-03-19 18:07:07 +0530

    Adding eureka client to project

--- demo/src/main/java/in/gangapatnam/demo/repository/EmployeeRepository.java
+++ demo/src/main/java/in/gangapatnam/demo/repository/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package in.gangapatnam.demo.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import in.gangapatnam.demo.model.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+	
 
 }
